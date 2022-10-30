91c5cc205f234c98a22314d768e30b9b73f60117
Jens Hauser <jenshauser1@gmail.com>
2022-10-30 23:27:51 +0100

    udacity project 2

--- src/data_check/test_data.py
+++ src/data_check/test_data.py
@@ -60,6 +60,15 @@ def test_similar_neigh_distrib(data: pd.DataFrame, ref_data: pd.DataFrame, kl_th
     assert scipy.stats.entropy(dist1, dist2, base=2) < kl_threshold
 
 
-########################################################
-# Implement here test_row_count and test_price_range   #
-########################################################
+def test_row_count(data):
+    """
+    Tests that the number of samples in the dataset is between 15000 and 1000000
+    """
+    assert 15000 < data.shape[0] < 1000000
+
+
+def test_price_range(data, min_price, max_price):
+    """
+    Checks that the value of price in the dataset is between 15000 and 1000000
+    """
+    assert data['price'].between(left=min_price, right=max_price, inclusive="both").all()
